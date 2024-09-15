39c17891a00602dbb9922f6f79d38a4e7a51cda3
Sebastian Visbal <164961287+ahig0ne@users.noreply.github.com>
2024-09-15 15:59:30 -0500

    Ejercicio21.py

--- Ejercicio21.py
+++ Ejercicio21.py
@@ -5,7 +5,7 @@ anguloUno = int(input('por favor ingrese primer angulo'))
 anguloDos = int(input('por favor ingrese segundo angulo'))
 anguloTres = int(input('por favor ingrese tercer angulo'))
 
-if  anguloUno!=anguloDos!=anguloTres<90:
+if  anguloUno<90 or anguloDos<90 or anguloTres<90:
     triangulo = 'agudo'
 elif anguloUno==90 or anguloDos==90 or anguloTres==90:
     triangulo = 'rectangulo'
